275dc0d16f99b66677f20afc3e01528354cb111a
Trần Văn Điệp <38648881+dieptv1999@users.noreply.github.com>
2024-06-19 09:10:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import { ScrollToTop } from "./components/ScrollToTop";
 import { Services } from "./components/Services";
 import { Sponsors } from "./components/Sponsors";
 import { Team } from "./components/Team";
-import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
 function App() {
